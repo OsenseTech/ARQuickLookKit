8c285b139ecae9533532c491a7c7fddba14d5a3d
蘇健豪 <jenhausu@osensetech.com>
2022-08-11 15:15:24 +0800

    feat: make VirtualReferenceObject copable

--- Sources/ARQuickLookKit/VirtualObject/VirtualReferenceObject.swift
+++ Sources/ARQuickLookKit/VirtualObject/VirtualReferenceObject.swift
@@ -32,5 +32,37 @@ public class VirtualReferenceObject: SCNReferenceNode, VirtualObjectProtocol {
     required init?(coder aDecoder: NSCoder) {
         fatalError()
     }
+    
+    init?(url: URL,
+          allowedAlignment: ARRaycastQuery.TargetAlignment,
+          anchor: ARAnchor? = nil,
+          raycast: ARTrackedRaycast? = nil,
+          shouldUpdateAnchor: Bool = false,
+          isPlaced: Bool = false,
+          objectType: ObjectType) {
+        self.allowedAlignment = allowedAlignment
+        self.anchor = anchor
+        self.raycast = raycast
+        self.shouldUpdateAnchor = shouldUpdateAnchor
+        self.isPlaced = isPlaced
+        self.objectType = objectType
+        
+        super.init(url: url)
+    }
+    
+    public override func copy(with zone: NSZone? = nil) -> Any {
+        let copyObject = VirtualReferenceObject(url: referenceURL,
+                                                allowedAlignment: allowedAlignment,
+                                                anchor: anchor,
+                                                raycast: raycast,
+                                                shouldUpdateAnchor: shouldUpdateAnchor,
+                                                isPlaced: isPlaced,
+                                                objectType: objectType)
+        copyObject?.geometry = self.geometry
+        copyObject?.name = self.name
+        copyObject?.scale = self.scale
+        
+        return copyObject as Any
+    }
 }
 
