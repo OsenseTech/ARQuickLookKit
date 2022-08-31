c16545db6cb13965b4507f9e335e972ec6b39256
蘇健豪 <jenhausu@osensetech.com>
2022-08-31 15:37:26 +0800

    feat: remove loadObjectWithKey

--- Sources/ARQuickLookKit/VirtualObject/VirtualObjectLoader.swift
+++ Sources/ARQuickLookKit/VirtualObject/VirtualObjectLoader.swift
@@ -15,21 +15,12 @@ public class VirtualObjectLoader {
     
     public var loadedObjects: [any VirtualObjectProtocol] = []
     
-    public var loadedObjectTable: [String: Int] = [:]
     public var isLoading = false
     
     public init() { }
     
     }
     
-    public func loadVirtualObject(_ object: VirtualReferenceObject, key: String) {
-        loadObject(object) { result in
-            if case let .success(object) = result {
-                self.loadedObjects.append(object)
-                self.loadedObjectTable[key] = self.loadedObjects.count - 1
-            }
-        }
-    }
     public func loadVirtualObject(_ object: VirtualReferenceObject, loadedHandler: @escaping (VirtualReferenceObject) -> Void) {
         isLoading = true
         loadedObjects.append(object)
@@ -42,11 +33,7 @@ public class VirtualObjectLoader {
         }
     }
     
-    public func getObject(for key: String) -> VirtualObjectProtocol? {
-        guard let index = loadedObjectTable[key] else { return nil }
-        let object = loadedObjects[index]
         
-        return object
     }
     
 }
